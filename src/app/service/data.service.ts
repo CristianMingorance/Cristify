56f8ba265da5165649a39982f8ad10c84d54b6b5
Cristian <cristian.mm0150@gmail.com>
2019-08-06 11:20:55 +0200

    changed enviroment vars

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,6 +1,7 @@
 import {
   Injectable
 } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -10,38 +11,38 @@ export class DataService {
 
   catalogoGrupos = [{
       titulo: "Bella Ciao",
-      imagen: "../../assets/img/grupo1.jpg",
+      imagen: environment.img_prefix + "assets/img/grupo1.jpg",
       grupo: "Modena City Ramblers",
-      video: "../../assets/img/bella_ciao.mp4",
+      video: environment.img_prefix + "assets/img/bella_ciao.mp4",
     },
     {
       titulo: "Humo",
-      imagen: "../../assets/img/grupo2.jpg",
+      imagen: environment.img_prefix + "assets/img/grupo2.jpg",
       grupo: "Jarabe de Palo",
-      video: "../../assets/img/humo.mp4" 
+      video: environment.img_prefix + "assets/img/humo.mp4" 
     },
     {
       titulo: "Friday i'm in Love",
-      imagen: "../../assets/img/grupo3.jpg",
+      imagen: environment.img_prefix + "assets/img/grupo3.jpg",
       grupo: "The Cure",
-      video: "../../assets/img/the_cure.mp4" 
+      video: environment.img_prefix + "assets/img/the_cure.mp4" 
     } ,{
       titulo: "Chan Chan",
-      imagen: "../../assets/img/grupo4.jpg",
+      imagen: environment.img_prefix + "assets/img/grupo4.jpg",
       grupo: "Buena Vista Social Club",
-      video: "../../assets/img/chan_chan.mp4"
+      video: environment.img_prefix + "assets/img/chan_chan.mp4"
     },
     {
       titulo: "Guantanamera",
-      imagen: "../../assets/img/grupo5.jpg",
+      imagen: environment.img_prefix + "assets/img/grupo5.jpg",
       grupo: "Compay Segundo",
-      video: "../../assets/img/guantanamera.mp4"
+      video: environment.img_prefix + "assets/img/guantanamera.mp4"
     },
     {
       titulo: "Pastillas de Freno",
-      imagen: "../../assets/img/grupo6.jpg",
+      imagen: environment.img_prefix + "assets/img/grupo6.jpg",
       grupo: "Estopa",
-      video: "../../assets/img/pastillas_de_freno.mp4"
+      video: environment.img_prefix + "assets/img/pastillas_de_freno.mp4"
     },
    ]
 
